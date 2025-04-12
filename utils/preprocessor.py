2716aafbc0dc91160c88fa4d46bd01c4f70b7663
RubabFatima8887 <109766470+RubabFatima8887@users.noreply.github.com>
2025-04-12 17:56:05 +0500

    Delete utils directory

--- utils/preprocessor.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-
-def prepare_features(data):
-    """Convert text to features and split into train/test sets"""
-    tfidf = TfidfVectorizer()
-    features = tfidf.fit_transform(data['Body'])
-    labels = data['Label']
-    return train_test_split(features, labels, test_size=0.2, random_state=42)
